6c53cc11c148f9a4f9d26b5bf9e2f33c2366256d
TechXNinjas <thetechxninjas@gmail.com>
2025-04-16 20:59:19 +0530

    Create 1st-lab-6th-game.sh

---
+++ 1st-lab-6th-game.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+UNDERLINE=`tput smul`
+RESET=`tput sgr0`
+
+BRIGHT_GREEN="${GREEN}"
+BRIGHT_YELLOW="${YELLOW}"
+BRIGHT_RED="${RED}"
+BRIGHT_BLUE="${BLUE}"
+BRIGHT_WHITE="${WHITE}"
+BRIGHT_PURPLE="${MAGENTA}"
+BRIGHT_CYAN="${CYAN}"
+RESET_FORMAT="${RESET}"
+
+# 💡 Start-Up Banner 
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo -e "${BRIGHT_CYAN}${BOLD}       6th Game: Level 1: Healthcare API Lab           ${RESET}"
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo ""
+
+# 🚀 Task Execution Init
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo -e "${BRIGHT_CYAN}${BOLD}         🚀 INITIATING THE TASK EXECUTION...           ${RESET}"
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo ""
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export PROJECT_NUMBER=$(gcloud projects list --filter=projectId:$PROJECT_ID \
+  --format="value(projectNumber)")
+export DATASET_ID=dataset1
+export FHIR_STORE_ID=fhirstore1
+export TOPIC=fhir-topic
+export HL7_STORE_ID=hl7v2store1
+
+gcloud services enable healthcare.googleapis.com
+
+sleep 20
+
+gcloud pubsub topics create $TOPIC
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset $PROJECT_ID:$DATASET_ID
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset-de-id $PROJECT_ID:de_id
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.dataEditor
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.jobUser
+
+gcloud healthcare datasets create $DATASET_ID \
+--location=$LOCATION
+
+gcloud healthcare fhir-stores create $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores update $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --pubsub-topic=projects/$PROJECT_ID/topics/$TOPIC
+
+gcloud healthcare fhir-stores create de_id \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores import gcs $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--gcs-uri=gs://spls/gsp457/fhir_devdays_gcp/fhir1/* \
+--content-structure=BUNDLE_PRETTY
+
+gcloud healthcare fhir-stores export bq $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.$DATASET_ID \
+--schema-type=analytics
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+sleep 180
+
+gcloud healthcare fhir-stores export bq de_id \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.de_id \
+--schema-type=analytics
+
+# ✅ Completion Message
+echo "${BRIGHT_GREEN}${BOLD}🎉===========================================================${RESET_FORMAT}"
+echo "${BRIGHT_GREEN}${BOLD}             ✅ YOU'VE SUCCESSFULLY COMPLETED THE LAB!         ${RESET_FORMAT}"
+echo "${BRIGHT_GREEN}${BOLD}🎉===========================================================${RESET_FORMAT}"
+echo ""
+
+# 📢 CTA
+echo -e "${BRIGHT_YELLOW}${BOLD}🔔 Follow for more labs & tutorials:${RESET}"
+echo -e "${BRIGHT_RED}${BOLD}YouTube Channel:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.youtube.com/@TechXNinjas${RESET}"
+echo -e "${BRIGHT_WHITE}${BOLD}Follow me on LinkedIn:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.linkedin.com/in/iaadillatif${RESET}"
+echo -e "${BRIGHT_WHITE}${BOLD}LinkedIn Page:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.linkedin.com/company/techxninjas${RESET}"
+echo -e "${BRIGHT_WHITE}${BOLD}Join WhatsApp Group:${RESET} ${BRIGHT_GREEN}${UNDERLINE}https://chat.whatsapp.com/HosxDxImviICAwizHaXXbu${RESET}"
+echo ""

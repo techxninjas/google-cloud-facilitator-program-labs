b4d64a73a93874b8220205113027fd8a3de24e2a
TechXNinjas <thetechxninjas@gmail.com>
2025-04-16 18:52:04 +0530

    Update gsp093.sh

--- gsp093.sh
+++ gsp093.sh
@@ -105,9 +105,16 @@ if [[ "$task2_check" != "Y" && "$task2_check" != "y" ]]; then
   exit 1
 fi
 
+# ✅ Completion Message
+echo "${BRIGHT_GREEN}${BOLD}🎉===========================================================${RESET_FORMAT}"
+echo "${BRIGHT_GREEN}${BOLD}             ✅ YOU'VE SUCCESSFULLY COMPLETED THE LAB!         ${RESET_FORMAT}"
+echo "${BRIGHT_GREEN}${BOLD}🎉===========================================================${RESET_FORMAT}"
+echo ""
+
 # 📢 CTA
 echo -e "${BRIGHT_YELLOW}${BOLD}🔔 Follow for more labs & tutorials:${RESET}"
 echo -e "${BRIGHT_RED}${BOLD}YouTube Channel:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.youtube.com/@TechXNinjas${RESET}"
+echo -e "${BRIGHT_WHITE}${BOLD}Follow me on LinkedIn:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.linkedin.com/in/iaadillatif${RESET}"
 echo -e "${BRIGHT_WHITE}${BOLD}LinkedIn Page:${RESET} ${BRIGHT_BLUE}${UNDERLINE}https://www.linkedin.com/company/techxninjas${RESET}"
-echo -e "${BRIGHT_WHITE}${BOLD}Join WhatsApp Group:${RESET} ${BRIGHT_GREEN}${UNDERLINE}https://chat.whatsapp.com/BZczJZSamtX144BCTagYxk${RESET}"
+echo -e "${BRIGHT_WHITE}${BOLD}Join WhatsApp Group:${RESET} ${BRIGHT_GREEN}${UNDERLINE}https://chat.whatsapp.com/HosxDxImviICAwizHaXXbu${RESET}"
 echo ""

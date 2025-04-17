97189b4021d097089efca504e0d6a9af95298481
TechXNinjas <thetechxninjas@gmail.com>
2025-04-17 20:14:11 +0530

    Create 1st-lab-6th-game2.sh

---
+++ 1st-lab-6th-game2.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+UNDERLINE=`tput smul`
+RESET=`tput sgr0`
+
+BRIGHT_GREEN="${GREEN}"
+BRIGHT_YELLOW="${YELLOW}"
+BRIGHT_RED="${RED}"
+BRIGHT_BLUE="${BLUE}"
+BRIGHT_WHITE="${WHITE}"
+BRIGHT_PURPLE="${MAGENTA}"
+BRIGHT_CYAN="${CYAN}"
+RESET_FORMAT="${RESET}"
+
+# 💡 Start-Up Banner 
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo -e "${BRIGHT_CYAN}${BOLD}                    6th Game: Lab 1             ${RESET}"
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo ""
+
+# 🚀 Task Execution Init
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo -e "${BRIGHT_CYAN}${BOLD}         🚀 INITIATING THE TASK EXECUTION...           ${RESET}"
+echo -e "${BRIGHT_PURPLE}${BOLD}-------------------------------------------------------${RESET}"
+echo ""
+
+read -p "${YELLOW_TEXT}${BOLD_TEXT}Enter the location (Check your Lab and type the Location here): ${RESET_FORMAT}" LOCATION
+export LOCATION=$LOCATION
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export PROJECT_NUMBER=$(gcloud projects list --filter=projectId:$PROJECT_ID \
+  --format="value(projectNumber)")
+export DATASET_ID=dataset1
+export FHIR_STORE_ID=fhirstore1
+export TOPIC=fhir-topic
+export HL7_STORE_ID=hl7v2store1
+
+gcloud healthcare fhir-stores export bq de_id \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.de_id \
+--schema-type=analytics

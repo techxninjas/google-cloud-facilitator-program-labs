ffb2ec3995418d3ce9dd94fef731d5b7f2714496
TechXNinjas <thetechxninjas@gmail.com>
2025-05-02 19:06:13 +0530

    Create arc104.sh

---
+++ arc104.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+# 💡 Start-Up Banner
+echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}                 App Engine: 3 Ways: Challenge Lab                     ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
+echo ""
+
+# 🌍 Fetching Region
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔄 Fetching Region...${RESET_FORMAT}"
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+echo ""
+
+# 🗺️ Fetching Zone
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔄 Fetching Zone...${RESET_FORMAT}"
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+echo ""
+
+# 🆔 Fetching Project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔍 Fetching Project ID...${RESET_FORMAT}"
+PROJECT_ID=`gcloud config get-value project`
+echo ""
+
+# 🔢 Fetching Project Number
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔍 Fetching Project Number...${RESET_FORMAT}"
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+echo ""
+
+# 💡 Start-Up Banner
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}         🚀 INITIATING THE TASK EXECUTION...          ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
+echo ""
+
+read -p "Enter Cloud Function Name: " FUNCTION_NAME
+export HTTP_FUNCTION=$HTTP_FUNCTION
+echo
+
+read -p "Enter HTTP Function Name: " HTTP_FUNCTION
+export FUNCTION_NAME=$FUNCTION_NAME
+
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Enabling the APIs...${RESET_FORMAT}"
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
+
+sleep 30
+
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
+echo
+
+gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID
+
+export BUCKET="gs://$DEVSHELL_PROJECT_ID"
+
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Creating directory and files for cloud event trigger function...${RESET_FORMAT}"
+mkdir ~/$FUNCTION_NAME && cd $_
+touch index.js && touch package.json
+
+cat > index.js <<EOF
+const functions = require('@google-cloud/functions-framework');
+functions.cloudEvent('$FUNCTION_NAME', (cloudevent) => {
+  console.log('A new event in your Cloud Storage bucket has been logged!');
+  console.log(cloudevent);
+});
+EOF
+
+cat > package.json <<EOF
+{
+  "name": "nodejs-functions-gen2-codelab",
+  "version": "0.0.1",
+  "main": "index.js",
+  "dependencies": {
+    "@google-cloud/functions-framework": "^2.0.0"
+  }
+}
+EOF
+
+deploy_function() {
+  gcloud functions deploy $FUNCTION_NAME \
+  --gen2 \
+  --runtime nodejs20 \
+  --entry-point $FUNCTION_NAME \
+  --source . \
+  --region $REGION \
+  --trigger-bucket $BUCKET \
+  --trigger-location $REGION \
+  --max-instances 2 \
+  --quiet
+}
+
+# Loop until the Cloud Run service is created
+echo
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $FUNCTION_NAME --region $REGION &> /dev/null; then
+    echo "${GREEN_TEXT}${BOLD_TEXT}Cloud Event function deployed successfully!${RESET_FORMAT}"
+    break
+  else
+    echo "${YELLOW_TEXT}${BOLD_TEXT}Waiting for Cloud Event function to be created...${RESET_FORMAT}"
+    sleep 10
+  fi
+done
+
+cd ..
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating directory and files for HTTP trigger function...${RESET_FORMAT}"
+echo
+
+mkdir ~/HTTP_FUNCTION && cd $_
+touch index.js && touch package.json
+
+cat > index.js <<EOF
+const functions = require('@google-cloud/functions-framework');
+functions.http('$HTTP_FUNCTION', (req, res) => {
+  res.status(200).send('awesome lab');
+});
+EOF
+
+
+cat > package.json <<EOF
+{
+  "name": "nodejs-functions-gen2-codelab",
+  "version": "0.0.1",
+  "main": "index.js",
+  "dependencies": {
+    "@google-cloud/functions-framework": "^2.0.0"
+  }
+}
+EOF
+
+deploy_function() {
+  gcloud functions deploy $HTTP_FUNCTION \
+  --gen2 \
+  --runtime nodejs20 \
+  --entry-point $HTTP_FUNCTION \
+  --source . \
+  --region $REGION \
+  --trigger-http \
+  --timeout 600s \
+  --max-instances 2 \
+  --min-instances 1 \
+  --quiet
+}
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}Deploying HTTP function...${RESET_FORMAT}"
+echo
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $HTTP_FUNCTION --region $REGION &> /dev/null; then
+    echo "${GREEN_TEXT}${BOLD_TEXT}HTTP function deployed successfully!${RESET_FORMAT}"
+    break
+  else
+    echo "${BLUE_TEXT}${BOLD_TEXT}Waiting for HTTP function to be created...${RESET_FORMAT}"
+    sleep 10
+  fi
+done
+
+# ✅ Completion Message
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}               ✅ ALL TASKS COMPLETED SUCCESSFULLY!            ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo ""
+echo "${GREEN_TEXT}${BOLD_TEXT} ✔ Please check your progress."
+echo "${GREEN_TEXT}${BOLD_TEXT} If it will be not completed, try again for successfully completion of the Assessment."
+echo ""
+
+for i in {1..20}; do
+    echo -ne "${CYAN_TEXT}⏳ ${i}/20 seconds to check your progress\r${RESET_FORMAT}"
+    sleep 1
+done
+echo
+
+remove_temp_files() {
+    echo "${YELLOW_TEXT}${BOLD_TEXT}Cleaning up temporary files...${RESET}"
+    for file in *; do
+        if [[ "$file" == gsp* || "$file" == arc* || "$file" == shell* ]]; then
+            [[ -f "$file" ]] && rm "$file" && echo "Removed: $file"
+        fi
+    done
+}
+remove_temp_files
+echo ""
+
+# ✅ Completion Message
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}          ✅ YOU'VE SUCCESSFULLY COMPLETED THE LAB!          ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo ""
+
+# 📢 CTA Section
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}🔔 Follow for more labs & tutorials:${RESET_FORMAT}"
+echo ""
+echo -e "${WHITE_TEXT}${BOLD_TEXT}YouTube Channel:${RESET_FORMAT} ${BLUE_TEXT}${UNDERLINE}https://www.youtube.com/@TechXNinjas${RESET_FORMAT}"
+echo -e "${WHITE_TEXT}${BOLD_TEXT}Join WhatsApp Group:${RESET_FORMAT} ${GREEN_TEXT}${UNDERLINE}https://chat.whatsapp.com/HosxDxImviICAwizHaXXbu${RESET_FORMAT}"
+echo ""

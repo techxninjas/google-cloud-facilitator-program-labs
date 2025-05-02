9d8f500871cdb7d2b53e433e479b9ea910b4c0e4
TechXNinjas <thetechxninjas@gmail.com>
2025-05-03 02:24:52 +0530

    Update gsp125_2.sh

--- gsp125_2.sh
+++ gsp125_2.sh
@@ -18,7 +18,7 @@ clear
 
 # 💡 Start-Up Banner
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}         Speaking with a Webpage - Streaming Speech Transcripts         ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}    Speaking with a Webpage - Streaming Speech Transcripts - part 2      ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
 echo ""
 
@@ -46,33 +46,12 @@ echo ""
 
 # 💡 Start-Up Banner
 echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}         🚀 INITIATING THE TASK EXECUTION...          ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}     🚀 INITIATING THE REMAINING TASK EXECUTION...     ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
 echo ""
 
 echo
-echo "${BLUE_TEXT}${BOLD_TEXT}Creating a new VM instance...${RESET_FORMAT}"
-echo "${YELLOW_TEXT}${BOLD_TEXT}This will take a few moments. So, please wait.${RESET_FORMAT}"
-gcloud compute instances create speaking-with-a-webpage --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD  --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name=speaking-with-a-webpage,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230711,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
-
-for i in {1..30}; do
-    echo -ne "${CYAN_TEXT}⏳ ${i}/30 seconds waiting for the pre-requisuites...\r${RESET_FORMAT}"
-    sleep 1
-done
-echo
-
-echo
-echo "${BLUE_TEXT}${BOLD_TEXT}Connecting to the VM instance via SSH and setting up the environment...${RESET_FORMAT}"
-gcloud compute ssh "speaking-with-a-webpage" --zone "$ZONE" --project "$DEVSHELL_PROJECT_ID" --quiet --command 'sudo apt update && sudo apt install git -y && sudo apt-get install -y maven openjdk-11-jdk && git clone https://github.com/googlecodelabs/speaking-with-a-webpage.git && gcloud compute firewall-rules create dev-ports --allow=tcp:8443 --source-ranges=0.0.0.0/0 && cd ~/speaking-with-a-webpage/01-hello-https && mvn clean jetty:run' 
-
-for i in {1..10}; do
-    echo -ne "${CYAN_TEXT}⏳ ${i}/10 seconds waiting for the pre-requisuites...\r${RESET_FORMAT}"
-    sleep 1
-done
-echo
-
-echo
-echo "${BLUE_TEXT}${BOLD_TEXT}Stopping any running Jetty server on the VM instance...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Stopping any running Jetty server on the VM instance...${RESET_FORMAT}"
 gcloud compute ssh "speaking-with-a-webpage" --zone "$ZONE" --project "$DEVSHELL_PROJECT_ID" --quiet --command "pkill -f 'java.*jetty'"
 
 for i in {1..10}; do
@@ -82,7 +61,7 @@ done
 echo
 
 echo
-echo "${BLUE_TEXT}${BOLD_TEXT}Starting the WebAudio application on the VM instance...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Starting the WebAudio application on the VM instance...${RESET_FORMAT}"
 gcloud compute ssh "speaking-with-a-webpage" --zone "$ZONE" --project "$DEVSHELL_PROJECT_ID" --quiet --command "cd ~/speaking-with-a-webpage/02-webaudio && mvn clean jetty:run"
 
 echo

228efde4bb248113824d2c0733f523ecdc6c0cef
TechXNinjas <thetechxninjas@gmail.com>
2025-05-03 02:24:55 +0530

    Create gsp125_1.sh

---
+++ gsp125_1.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+clear
+
+# 💡 Start-Up Banner
+echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}    Speaking with a Webpage - Streaming Speech Transcripts - part 1      ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
+echo ""
+
+# 🌍 Fetching Region
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔄 Fetching Region...${RESET_FORMAT}"
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+echo ""
+
+# 🗺️ Fetching Zone
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔄 Fetching Zone...${RESET_FORMAT}"
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+echo ""
+
+# 🆔 Fetching Project ID
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔍 Fetching Project ID...${RESET_FORMAT}"
+PROJECT_ID=`gcloud config get-value project`
+echo ""
+
+# 🔢 Fetching Project Number
+echo "${MAGENTA_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}🔍 Fetching Project Number...${RESET_FORMAT}"
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+echo ""
+
+# 💡 Start-Up Banner
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}         🚀 INITIATING THE TASK EXECUTION...          ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
+echo ""
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Creating a new VM instance...${RESET_FORMAT}"
+echo "${YELLOW_TEXT}${BOLD_TEXT}This will take a few moments. So, please wait.${RESET_FORMAT}"
+gcloud compute instances create speaking-with-a-webpage --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD  --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name=speaking-with-a-webpage,image=projects/debian-cloud/global/images/debian-11-bullseye-v20230711,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+for i in {1..30}; do
+    echo -ne "${CYAN_TEXT}⏳ ${i}/30 seconds waiting for the pre-requisuites...\r${RESET_FORMAT}"
+    sleep 1
+done
+echo
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}---> Connecting to the VM instance via SSH and setting up the environment...${RESET_FORMAT}"
+gcloud compute ssh "speaking-with-a-webpage" --zone "$ZONE" --project "$DEVSHELL_PROJECT_ID" --quiet --command 'sudo apt update && sudo apt install git -y && sudo apt-get install -y maven openjdk-11-jdk && git clone https://github.com/googlecodelabs/speaking-with-a-webpage.git && gcloud compute firewall-rules create dev-ports --allow=tcp:8443 --source-ranges=0.0.0.0/0 && cd ~/speaking-with-a-webpage/01-hello-https && mvn clean jetty:run' 
+
+for i in {1..10}; do
+    echo -ne "${CYAN_TEXT}⏳ ${i}/10 seconds waiting for the pre-requisuites...\r${RESET_FORMAT}"
+    sleep 1
+done
+echo
+
+# ✅ Completion Message
+echo
+echo "${RED_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}                 All tasks are not completed...            ${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}🎉===========================================================${RESET_FORMAT}"
+echo ""
+echo ""
+echo "${GREEN_TEXT}${BOLD_TEXT}------> See the video carefully:"
+echo "${GREEN_TEXT}${BOLD_TEXT}------> And do the next steps as follows in the video."
+echo ""
+
+# 📢 CTA Section
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}🔔 Follow for more labs & tutorials:${RESET_FORMAT}"
+echo ""
+echo -e "${WHITE_TEXT}${BOLD_TEXT}YouTube Channel:${RESET_FORMAT} ${BLUE_TEXT}${UNDERLINE}https://www.youtube.com/@TechXNinjas${RESET_FORMAT}"
+echo -e "${WHITE_TEXT}${BOLD_TEXT}Join WhatsApp Group:${RESET_FORMAT} ${GREEN_TEXT}${UNDERLINE}https://chat.whatsapp.com/HosxDxImviICAwizHaXXbu${RESET_FORMAT}"
+echo ""

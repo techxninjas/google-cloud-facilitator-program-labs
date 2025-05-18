3d675891c67b1aa3f5ba14fe40fe90d87d55c9b6
TechXNinjas <thetechxninjas@gmail.com>
2025-05-19 01:51:34 +0530

    Update gsp199.sh

--- gsp199.sh
+++ gsp199.sh
@@ -18,7 +18,7 @@ clear
 
 # 💡 Start-Up Banner
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}        Store, Process, and Manage Data on Google Cloud: Challenge Lab      ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}               Service Accounts and Roles: Fundamentals          ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
 echo ""
 

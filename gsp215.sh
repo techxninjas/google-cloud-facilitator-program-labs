ecfb8f77549a269678a3969bcca01b0c90bd4f98
TechXNinjas <thetechxninjas@gmail.com>
2025-05-21 12:29:18 +0530

    Update gsp215.sh

--- gsp215.sh
+++ gsp215.sh
@@ -18,7 +18,7 @@ clear
 
 # 💡 Start-Up Banner
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}        Store, Process, and Manage Data on Google Cloud: Challenge Lab      ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}             Application Load Balancer with Cloud Armor         ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
 echo ""
 

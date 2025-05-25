86286100dd3a342ec0361c9dfc37d8f34eea70f8
TechXNinjas <thetechxninjas@gmail.com>
2025-05-25 17:52:06 +0530

    Update arc123.sh

--- arc123.sh
+++ arc123.sh
@@ -17,10 +17,9 @@ UNDERLINE_TEXT=$'\033[4m'
 clear
 
 # 💡 Start-Up Banner
-echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}     10th Game: Skills Boost Arcade Certification Zone April 2025      ${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}        7th Lab: Tag and Discover BigLake Data: Challenge Lab          ${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}      Tag and Discover BigLake Data: Challenge Lab      ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}-------------------------------------------------------${RESET_FORMAT}"
 echo ""
 
 # 🌍 Fetching Region

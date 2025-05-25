06016b09cf0745d4c0c0f66396ddc7ff8374c522
TechXNinjas <thetechxninjas@gmail.com>
2025-05-25 20:29:29 +0530

    Update arc115.sh

--- arc115.sh
+++ arc115.sh
@@ -177,7 +177,7 @@ echo
 echo "${CYAN_TEXT}${BOLD_TEXT}Configuration for labels:${RESET_FORMAT}"
 echo "${BLUE_TEXT}${BOLD_TEXT}Label Name: techxninjas"${RESET_FORMAT}"
 echo "${BLUE_TEXT}${BOLD_TEXT}Field Name: textPayload"${RESET_FORMAT}"
-echo "${BLUE_TEXT}${BOLD_TEXT}Regular Expression: execution took (\d+)"${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Regular Expression: execution took (\\d+)"${RESET_FORMAT}"
 
 for i in {1..60}; do
     echo -ne "${CYAN_TEXT}⏳ ${i}/60 seconds for doing this Task\r${RESET_FORMAT}"

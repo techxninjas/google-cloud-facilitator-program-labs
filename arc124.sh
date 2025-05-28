b61201cf437ddba2893651d254c063dba8d65942
TechXNinjas <thetechxninjas@gmail.com>
2025-05-29 01:35:04 +0530

    Update arc124.sh

--- arc124.sh
+++ arc124.sh
@@ -18,7 +18,7 @@ clear
 
 # 💡 Start-Up Banner
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}         LAB_NAME       ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}         Networking Fundamentals on Google Cloud: Challenge Lab       ${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}-----------------------------------------------------------------------${RESET_FORMAT}"
 echo ""
 
